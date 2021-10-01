d1af8155cb4c3588da12a306e22b8bb9e1988f5d
Abhishek Kapoor <42829419+Abhishek9111@users.noreply.github.com>
2021-10-01 23:33:29 +0530

    element that appears once
    The following program finds the element that appears only once from the sorted array.

---
+++ CPP/ElementThatAppearsonce.cpp
@@ -0,0 +1,45 @@
+/*******************************
+The following program finds the element that appears only once from the sorted array.
+*********************/
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int search(int A[], int N){
+	    //code
+	    int res=0;
+	    for(int i=0;i<N;i++){
+	        res = res^A[i];
+	    }
+	    return res;
+	}
+
+};
+
+// { Driver Code Starts.
+
+// Driver program
+int main()
+{
+    int t,len;
+    cin>>t;
+    while(t--)
+    {
+        cin>>len;
+        int arr[len];
+        for(int i=0;i<len;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.search(arr, len)<<'\n';
+    }
+    return 0;
+}
+  // } Driver Code Ends
